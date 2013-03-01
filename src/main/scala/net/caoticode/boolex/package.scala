4bbb5ed5b18344717a7a4d7392b864253cd1b649
Daniel Camarda <0xcaos@gmail.com>
2013-03-01 02:39:49 +0100

    boolean conversion logic moved from BoolexOPS to any2Boolean

--- src/main/scala/net/caoticode/boolex/package.scala
+++ src/main/scala/net/caoticode/boolex/package.scala
@@ -5,7 +5,10 @@ package object boolex {
     def &&&[A >: T](op2: => A): A = if (toBool) op2 else op1
     def |||[A >: T](op2: => A): A = if (toBool) op1 else op2
     
-    def toBool: Boolean = op1 match {
+    def toBool: Boolean = any2Boolean(op1)
+  }
+  
+  implicit def any2Boolean(elem: Any): Boolean = elem match {
       case e: BoolexOPS[_] => e.toBool
       case e: String => !(e isEmpty)
       case e: Number => e != 0
@@ -13,7 +16,4 @@ package object boolex {
       case e: Traversable[_] => !(e isEmpty)
       case e => e != null && e != None
     }
-  }
-  
-  implicit def any2Boolean(elem: Any): Boolean = new BoolexOPS(elem).toBool
 }
